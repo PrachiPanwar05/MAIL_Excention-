42ff4212fac9b58f10298ef171710083b1e999ba
PRACHI <76863155+PrachiPanwar05@users.noreply.github.com>
2023-02-17 18:02:13 +0530

    Add files via upload

---
+++ Mail EXCENTION/Mailer.py
@@ -0,0 +1,43 @@
+from flask import Flask,render_template,request
+from mailbox import Message
+#import Mail as Mail
+from flask_mail import *
+from random import *
+
+app = Flask(__name__)
+mail = Mail(app)
+
+
+app.config["MAIL_SERVER"]='smtp.gmail.com'
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USERNAME"] = 'prachipanwar05@gmail.com'
+app.config['MAIL_PASSWORD'] = "hwwvuwqbcvtzubek"
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True 
+
+mail = Mail(app)
+otp = randint(000000,999999)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/verify',methods = ["POST"])
+def verify():
+    email = request.form["email"]
+    msg = Message('OTP', sender = 'prachipanwar05@gmail.com', recipients = [email])
+    msg.body = str(otp)                                                                     
+    mail.send(msg)
+    return render_template('verify.html')
+
+@app.route('/validate',methods=["POST"])
+def validate():
+    user_otp = request.form['otp']
+    if otp == int(user_otp):
+        return "<h3>Email verified successfully</h3>"
+    return "<h3>failure</h3>"
+
+if __name__ == '__main__':
+    app.run(debug = True, port = 1243)
+
+
